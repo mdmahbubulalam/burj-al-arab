ac5475a63af727aa662113e02aee9a1888a77402
mdmahbubulalam <alam.diu13@gmail.com>
2021-04-07 14:05:25 +0600

    second commit

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB-YUUUW9A1EsT5mjej90A4Dv44ABLdNKE",
+    authDomain: "burj-al-arab-8ab07.firebaseapp.com",
+    projectId: "burj-al-arab-8ab07",
+    storageBucket: "burj-al-arab-8ab07.appspot.com",
+    messagingSenderId: "620373013944",
+    appId: "1:620373013944:web:482852471eaf5f531c4b4d"
+  };
+
+  export default firebaseConfig;
